95f4e05e4006b6ba838875b5d568f2b450b4b47f
emanuelplopu <emanuel.plopu@sepa.at>
2016-04-25 17:50:33 +0200

    sanitary importer
    sanitary importer

--- local/modules/HookKonfigurator/Import/SanitaryImport.php
+++ local/modules/HookKonfigurator/Import/SanitaryImport.php
@@ -14,6 +14,7 @@ use Thelia\Model\Product;
 use HookKonfigurator\Model\Montage;
 use HookKonfigurator\Model\MontageConstraints;
 use HookKonfigurator\Model\Constraints;
+use Thelia\Model\ProductImage;
 
 /**
  * Class SanitaryImport
@@ -32,142 +33,132 @@ class SanitaryImport extends ImportHandler
     {
         $errors = [];
         $log = Tlog::getInstance ();
-        $log->debug(" service_import ");
+        $log->debug(" sanitary_import ");
         
+
         $productQuerry = ProductQuery::create ();
-        $constraintsQuerry = ConstraintsQuery::create ();
+        
         
         $currentDate = date ( "Y-m-d H:i:s" );
-        $serviceCategoryId = 12;
+        $heizungCategoryId = 11;
         
         $i = 0;
-        
-        while (null !== $row = $data->popRow()) {       
-        	$log->debug( "importing_service ".$i.implode(" ",$row));
 
+        while (null !== $row = $data->popRow()) {
+
+        	$log->debug(" importing_product_heizung ".$i.implode(" ",$row));
             $this->checkMandatoryColumns($row);
             
-            // id ref name description type price quantity unit extra_quantity_price constraints_values constraints_names currency duration
+            $sht_id = utf8_encode($row["sht_id"]);
+            $bild_name_wurzel = utf8_encode($row["bild_name_wurzel"]);
             
-            //excel saves csv as ansi, we need utf8
-            $ref = utf8_encode($row["ref"]);
-            $name = utf8_encode($row["name"]);
-            $description = utf8_encode($row["description"]);
-            $type = utf8_encode($row["type"]);
-            $unit = utf8_encode($row["unit"]);
-            $constraintsNames = utf8_encode($row["constraints_names"]);
-            $constraintsValues = $row["constraints_values"];
-            $quantity = $row["quantity"];
+            $marke = utf8_encode($row["marke"]);
+            $produkt_name = utf8_encode($row["produkt_name"]);
+            $detaillierte_beschreibung = utf8_encode($row["detaillierte_beschreibung"]);
+            $zusammengefaste_beschreibung = utf8_encode($row["zusammengefaste_beschreibung"]);
+            $produkt_merkmale = utf8_encode($row["produkt_merkmale"]);
+            $category = utf8_encode($row["kategorie"]);
+            $services = utf8_encode($row["services"]);
+            $dokumente = utf8_encode($row["dokumente"]);
+            $hersteller_produkt_url = utf8_encode($row["hersteller_produkt_url"]);
+            $farbe = utf8_encode($row["farbe"]);
+            $reuter_megabad_aktion = utf8_encode(str_replace(",", ".", $row["reuter_megabad_aktion"]));
             
-            //$log->debug ( " constraints " . $constraintsNames );
-            //number format, change "," to "."
-            $price = str_replace(",", ".", $row["price"]);
-            $extra_quantity_price = str_replace(",", ".", $row["extra_quantity_price"]);
-            $duration = $row["duration"];
-            		
-			//check for existing services
+            $anzahl_von_bilder = $row["anzahl_von_bilder"];
+            $gewicht = str_replace(",", ".", $row["gewicht"]);
+            $preis = str_replace(".", "", $row["preis"]);
+            $preis = str_replace(",", ".", $preis);
+            if($preis == "NULL") $preis = 0;
+            $gc_ek = $row["gc_ek"];
+
+            //check for existing services
             $productQuerry->clear ();
-            $productExists = count ( $productQuerry->findByRef ( $ref ) );
+            $productExists = count ( $productQuerry->findByRef ( $sht_id ) );
             
-            if ($productExists == 0) // product_numbers must be unique
-            	{
-            		$log->debug ( " service is new " );
+                    if ($productExists == 0) // product_numbers must be unique
+            {
+            	$log->debug ( " product_sanitary is new " );
             	$productThelia = new Product ();
-            	$productThelia->setRef ( $ref ); // must be unique
+            	$productThelia->setRef ( $sht_id ); // must be unique
             	$productThelia->setVisible ( 0 );
-            				
+            	
             	$productThelia->setCreatedAt ( $currentDate );
             	$productThelia->setUpdatedAt ( $currentDate );
             	$productThelia->setVersion ( 1 );
             	$productThelia->setVersionCreatedAt ( $currentDate );
             	$productThelia->setVersionCreatedBy ( "importer.1" );
-            	$productThelia->create ( $serviceCategoryId, $price, 1, 1, 'NULL', 20 );
-            	$log->debug ( " service is add as product " );
+            	$productThelia->create ( $heizungCategoryId, $preis, 1, 1, 'NULL', 20 );
+            	$log->debug ( " sanitary is add as product " );
             	
             	// product description
+            	$locale = "en_US";
             	$productI18n = new ProductI18n ();
             	$productI18n->setProduct ( $productThelia );
-            	$productI18n->setLocale ( "en_US" );
-            	$productI18n->setTitle ( $name );
-            	$productI18n->setDescription ( $description );
-            	$productI18n->setChapo ( " Zusammenfassung von " . $name );
-            	$productI18n->setPostscriptum ( " Merkmalen von " . $name );
+            	$productI18n->setLocale ( $locale );
+            	$productI18n->setTitle ( $produkt_name );
+            	$productI18n->setDescription ( $detaillierte_beschreibung );
+            	$productI18n->setChapo ( $zusammengefaste_beschreibung );
+            	$productI18n->setPostscriptum ( $produkt_merkmale );
             	$productI18n->save ();
-            	$log->debug ( " service i18n is added " );
+            	$log->debug ( " product_i18n ".$locale." is added ".$productI18n->__toString() );
             	$productThelia->addProductI18n ( $productI18n );
-            				
-            	$product_montage = new Montage ();
-            	$product_montage->setId ( $productThelia->getId () );
-            	$product_montage->setType ( $type );
             	
-            	if(is_numeric($quantity))
-            		$product_montage->setQuantity ( $quantity );
-            		$product_montage->setUnit ( $unit );
+            	$locale = "de_DE";
+            	$productI18n = new ProductI18n ();
+            	$productI18n->setProduct ( $productThelia );
+            	$productI18n->setLocale ( $locale );
+            	$productI18n->setTitle ( $produkt_name );
+            	$productI18n->setDescription ( $detaillierte_beschreibung );
+            	$productI18n->setChapo ( $zusammengefaste_beschreibung );
+            	$productI18n->setPostscriptum ( $produkt_merkmale );
+            	$productI18n->save ();
+            	$log->debug ( " product_i18n ".$locale." is added ".$productI18n->__toString() );
+            	$productThelia->addProductI18n ( $productI18n );
+            	/*$log->debug(" images ".$anzahl_von_bilder);
+            	for($image_index = 0; $image_index < $anzahl_von_bilder; $image_index++){
+            		$image_save_path = 'C:\Development\programs\xampp\htdocs\heizfabrik\local\media\images\product\\';
+            		$image_origin_path = 'C:\Development\programs\xampp\htdocs\heizfabrik\import\media\\';
             		
-            	if(is_numeric($extra_quantity_price))
-            		$product_montage->setExtraQuantityPrice ( $extra_quantity_price );
+            		
+            	$product_image_url = $image_origin_path.$bild_name_wurzel;
+            	if($product_image_url){
+            		// product image
+            		$product_image_url = $product_image_url."_".$image_index;
+            		$image_name = 'PROD_' . $productThelia->getRef()."_".$image_index.'.jpg';
+            
+            		try{
+            			$image_from_server =@file_get_contents ( $product_image_url );
+            		}
+            		catch (Exception $e) {
+            			$log->debug ("ProductImageException :".$e->getMessage());
+            		}
             	
-            	$product_montage->setDuration ( $duration );
-            	$log->debug ( "montageerror " . $product_montage->__toString () );
-            	$product_montage->save ();
-            						
-            	$constraintsNamesArray = explode ( ";", $constraintsNames );
-            	$constraintsValuesArray = explode ( ";", $constraintsValues );
-            
-            	for($i = 0; $i < sizeof ( $constraintsNamesArray ); $i ++){
-            
-            	if ($constraintsNamesArray [$i] != "NULL") {
-            								
-            		$montageConstraint = new MontageConstraints ();
-            		$montageConstraint->setMontageId ( $product_montage->getId () );
-            		$constraintValue = str_replace(",", ".", $constraintsValuesArray [$i]);
-            		$montageConstraint->setConstraintValue ( $constraintValue );
-
-            		$constraintsQuerry->clear ();
-            		$constraints_result = $constraintsQuerry->findByName ( $constraintsNamesArray [$i] );
-            		if(count($constraints_result)>0){ // old constraint
-            				$log->debug(" found ".count($constraints_result));
-            				$montageConstraint->setConstraintsId ( $constraints_result [0]->getId () );
-            				}
-            		else { // new constraint
-            			$constraint = new Constraints ();
-            			$constraint->setName ( $constraintsNamesArray [$i] );
-            			$constraint->setDescription ( " description of constraint " . $constraintsNamesArray [$i] );
-            			$log->debug(" constraintobj ".$constraint->__toString());
-            			$constraint->save ();
-            			$montageConstraint->setConstraintsId ( $constraint->getId () );
-            			}	
-            	 $montageConstraint->save();
+            		if($image_from_server){
+            			file_put_contents ( $image_save_path . $image_name, $image_from_server );
+            	
+            			$product_image = new ProductImage ();
+            			$product_image->setProduct ( $productThelia );
+            			$product_image->setVisible ( 1 );
+            			$product_image->setCreatedAt ( $currentDate );
+            			$product_image->setUpdatedAt ( $currentDate );
+            			$product_image->setFile ( $image_name );
+            			$product_image->save ();
+            	
+            			$productThelia->addProductImage ( $product_image );
+            		}
             	}
-            	}
-            	}
-            	else
-            	{
-            		$errors[] = $this->translator->trans( "Product reference number %ref is already in the database ",
-            				["%ref" => $row["ref"]] );
-            		$log->debug ( " ref number already in the database '" . $ref . "'" );
-            	}
-            
-            						
-         $this->importedRows++;
+            	}*/
 
-            /*
-            $obj = ProductSaleElementsQuery::create()->findPk($row["id"]);
-
-            if ($obj === null) {
-                $errors[] = $this->translator->trans(
-                    "The product sale element reference %id doesn't exist",
-                    ["%id" => $row["id"]] );
-            } else {
-                $obj->setQuantity($row["stock"]);
-
-                if (isset($row["ean"]) && !empty($row["ean"])) {
-                    $obj->setEanCode($row["ean"]);
-                }
-
-                $obj->save();
-                $this->importedRows++;
-            }*/
+            }
+            else             
+            {
+            	$errors[] = $this->translator->trans( "Product reference number %ref is already in the database ",
+            				["%ref" => $row["sht_id"]] );
+            	$log->debug ( " ref number already in the database '" . $sht_id . "'" );
+            	}     	
+        $this->importedRows++;
+        
+        $i++;
         }
 
         return $errors;
@@ -175,7 +166,9 @@ class SanitaryImport extends ImportHandler
 
     protected function getMandatoryColumns()
     {
-        return ["id", "ref","name","description","type","price","quantity","unit","extra_quantity_price","constraints_values","constraints_names","currency","duration"];
+        return ["product_id", "sht_id","hersteller_produkt_nummer","bild_name_wurzel","anzahl_von_bilder","marke","produkt_name",
+        		"detaillierte_beschreibung","zusammengefaste_beschreibung","produkt_merkmale","services","dokumente",
+        		"hersteller_produkt_url","gewicht","farbe","kategorie","reuter_megabad_aktion","reuter_megabad_preis","preis","gc_ek"];
     }
 
     /**
