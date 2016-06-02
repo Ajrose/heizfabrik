bc7d0f89b17ec69caea3faf1f8885d319fdf171f
Ajrose <aniarm21@yahoo.com>
2016-06-02 12:49:45 +0200

    priority

--- web/schedule/app/views/pjFrontPublic/elements/cart_layout2.php
+++ web/schedule/app/views/pjFrontPublic/elements/cart_layout2.php
@@ -6,20 +6,27 @@ if(!empty($cart))
 		<h3 class="pjAsAsideTitle"><?php __('front_selected_services'); ?></h3><!-- /.pjAsAsideTitle -->
 	</li><!-- /.list-group-item pjAsAsideService -->
 	<?php
+    $counter=0;
+    
+    if(count($cart)>3) array_splice($cart,2,-1);
 	foreach ($cart as $key => $value)
 	{
+        $counter++;
+  
 		list($cid, $date, $service_id, $start_ts, $end_ts, $employee_id) = explode("|", $key);
 	
 		$fixed_start_ts = $start_ts ;//+ $cart_arr[$service_id]['before'] * 60;
 		$fixed_end_ts = $end_ts ;//- $cart_arr[$service_id]['after'] * 60;
 		//echo "bla ".$key." bla";
+        
 		?>
 		<li class="list-group-item pjAsAsideService">
+            
 			<dl>
-				<dt class="pjAsServiceTitle"><?php /*echo pjSanitize::html($cart_arr[$service_id]['name']);*/?></dt>
+
 				
 				<dd>
-					<p><?php echo date($tpl['option_arr']['o_date_format'], strtotime($date));?></p>
+					<p><strong>Priorität<?php echo $counter; ?></strong> <?php echo  date($tpl['option_arr']['o_date_format'], strtotime($date));?></p>
 					<p><?php echo date($tpl['option_arr']['o_time_format'], $fixed_start_ts);?> - <?php echo date($tpl['option_arr']['o_time_format'], $fixed_end_ts);?></p>
 				</dd>
 			</dl>
@@ -29,11 +36,12 @@ if(!empty($cart))
 			</button>
 		</li><!-- /.list-group-item pjAsAsideService -->
 		<?php
+         
 	} 
 	?>
-	<li class="list-group-item pjAsAsideService">
+	<!--li class="list-group-item pjAsAsideService">
 		<a href="#" class="btn btn-default pjAsBtn pjAsBtnPrimary pjAsBtnGotoCheckout" data-start_ts="<?php echo $start_ts; ?>" data-end_ts="<?php echo $end_ts; ?>" data-date="<?php echo $date; ?>" data-service_id="<?php echo $service_id; ?>" data-employee_id="<?php echo $employee_id; ?>"><?php __('front_checkout');?></a>
-	</li><!-- /.list-group-item pjAsAsideService -->
+	</li--><!-- /.list-group-item pjAsAsideService -->
 	<?php
 } 
 ?>
